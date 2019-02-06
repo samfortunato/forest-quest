03bc7b292b0bc4a51e4a5a1e726aa0fb48fc53aa
Samuel Fortunato <samfortunato@gmail.com>
2019-02-06 11:47:02 -0500

    Add basic drawing of sprites

--- js/util/draw-util.js
+++ js/util/draw-util.js
@@ -1,4 +1,38 @@
-export const drawBeing = (ctx, being) => {
+export const drawShape = (being, ctx) => {
   ctx.fillStyle = being.color;
   ctx.fillRect(being.x, being.y, being.width, being.height);
+};
+
+// export const drawBeing = (being, ctx) => {
+//   ctx.drawImage(
+//     being.sprite,
+//     26,
+//     68,
+//     21,
+//     28,
+//     being.x,
+//     being.y,
+//     21,
+//     28
+//   );
+// };
+
+export const drawPlayer = (being, ctx) => {
+  ctx.drawImage(
+    being.sprite,
+    26, 68,
+    21, 28,
+    being.x, being.y,
+    21, 28
+  );
+};
+
+export const drawMonster = (being, ctx) => {
+  ctx.drawImage(
+    being.sprite,
+    27, 85,
+    19, 11,
+    being.x, being.y,
+    19, 11
+  );
 };
