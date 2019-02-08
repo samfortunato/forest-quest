ca4465ecd29c23edbb74520c313b2b8e97ef446a
Samuel Fortunato <samfortunato@gmail.com>
2019-02-08 17:28:23 -0500

    Change placement of some trees

--- js/beings/entities.js
+++ js/beings/entities.js
@@ -10,14 +10,13 @@ const entities = {
 
   boundaries: {
     tree1: new Tree(224, -96),
-    tree2: new Tree(352, 64),
+    tree2: new Tree(352, -64),
     tree3: new Tree(480, 0),
-    tree4: new Tree(608, 32),
+    tree4: new Tree(608, -64),
     tree5: new Tree(736, 64),
     tree6: new Tree(896, 0),
     tree7: new Tree(64, 160),
-    rock1: new Rock(160, 160),
-    treeTest: new Tree(300, 300)
+    rock1: new Rock(160, 160)
   },
 };
 
