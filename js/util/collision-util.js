77a171fbd66bff210514420f7ea77902b121bdbf
Samuel Fortunato <samfortunato@gmail.com>
2019-02-12 12:18:49 -0500

    Fix player/scenery collision

--- js/util/collision-util.js
+++ js/util/collision-util.js
@@ -1,6 +1,6 @@
 import Scenery from '../scenery/scenery';
-import Being from '../beings/being';
 import Player from '../beings/player';
+import BasicEnemy from '../beings/basic-enemy';
 
 export const collisionDetected = (entity1, entity2) => {
   if (entity1.x < (entity2.x + entity2.width) &&
@@ -18,28 +18,34 @@ export const wouldCollide = (direction, entity1, entity2) => {
   const entity1MoveInfo = {
     x: entity1.x,
     y: entity1.y,
+    width: entity1.width,
+    height: entity1.height,
     speed: entity1.speed,
     velocity: entity1.velocity
   };
 
+  const entity1MoveSpeed = (
+    entity1MoveInfo.speed * entity1MoveInfo.velocity
+  );
+
   switch (direction) {
     case 'up':
-      entity1MoveInfo.y -= (entity1MoveInfo.speed * entity1MoveInfo.velocity);
+      entity1MoveInfo.y -= entity1MoveSpeed;
 
       if (collisionDetected(entity1MoveInfo, entity2)) return true;
       break;
     case 'right':
-      entity1MoveInfo.x += (entity1MoveInfo.speed * entity1MoveInfo.velocity);
+      entity1MoveInfo.x += entity1MoveSpeed;
 
       if (collisionDetected(entity1MoveInfo, entity2)) return true;
       break;
     case 'down':
-      entity1MoveInfo.y += (entity1MoveInfo.speed * entity1MoveInfo.velocity);
+      entity1MoveInfo.y += entity1MoveSpeed;
 
       if (collisionDetected(entity1MoveInfo, entity2)) return true;
       break;
     case 'left':
-      entity1MoveInfo.x -= (entity1MoveInfo.speed * entity1MoveInfo.velocity);
+      entity1MoveInfo.x -= entity1MoveSpeed;
 
       if (collisionDetected(entity1MoveInfo, entity2)) return true;
       break;
@@ -48,27 +54,29 @@ export const wouldCollide = (direction, entity1, entity2) => {
   return false;
 };
 
-export const wouldCollideWithAny = (direction, entity, entities) => {
-  const collidesWithSingleEntity = (singleEntity) => {
-    if (singleEntity instanceof Player) {
-      return false;
-    } else if (singleEntity instanceof Scenery) {
-      const sceneryValues = {
-        x: singleEntity.boundaryX,
-        y: singleEntity.boundaryY,
-        width: singleEntity.boundaryWidth,
-        height: singleEntity.boundaryHeight
-      };
+export const wouldCollideWithSceneryObj = (direction, entity, sceneryObj) => {
+  const sceneryValues = {
+    x: sceneryObj.boundaryX,
+    y: sceneryObj.boundaryY,
+    width: sceneryObj.boundaryWidth,
+    height: sceneryObj.boundaryHeight
+  };
 
-      return wouldCollide(direction, entity, sceneryValues);
+  return wouldCollide(direction, entity, sceneryValues);
+};
+
+export const wouldCollideWithAny = (direction, entity, entities) => {
+  return Object.values(entities).some((otherEntity) => {
+    if (otherEntity.constructor === Object) {
+      return wouldCollideWithAny(direction, entity, otherEntity);
+    } else if (otherEntity instanceof Player) {
+      return false;
+    } else if (otherEntity instanceof BasicEnemy) {
+      return false;
+    } else if (otherEntity instanceof Scenery) {
+      return wouldCollideWithSceneryObj(direction, entity, otherEntity);
     } else {
-      return wouldCollide(direction, entity, singleEntity);
+      return wouldCollide(direction, entity, otherEntity);
     }
-  };
-  
-  const collidesWithAnyEntity = (otherEntity, i) => {
-    return Object.values(otherEntity).some(collidesWithSingleEntity);
-  };
-  
-  return Object.values(entities).some(collidesWithAnyEntity);
+  });
 };
